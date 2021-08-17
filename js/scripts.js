27364765ea3e2ddbb4452060e97c29c28a69a8d2
LukeHTHompson <lht14dv@icloud.com>
2021-08-17 14:09:29 -0400

    pokemonList Array Creation
    Created Array to hold info on pokemon.
    Added Bellsprout and its evolutions to the array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-alert('Hello World!');
-let favoriteFood="Pizza";
-document.write(favoriteFood);
+let pokemonList = [
+  { name: "Bellsprout",
+    height: 0.7,
+    types: ["Grass", "Poison"]
+    }
+  { name: "Weepinbell",
+    height: 1,
+    types: ["Grass", "Posion"]
+    }
+  { name: "Victreebel",
+    height: 1.7,
+    types: ["Grass", "Poison"]
+    }
+];
