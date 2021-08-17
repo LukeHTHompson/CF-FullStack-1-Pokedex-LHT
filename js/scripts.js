78a2f13e8814826af8c18c1045566bfc16b943ee
LukeHTHompson <lht14dv@icloud.com>
2021-08-17 15:28:55 -0400

    Pokemon Name/Height Loop
    Conditional within loop will tack on "That's one TALL Pokemon" for any pokemon taller than 1.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
 let pokemonList = [
   { name: "Bellsprout",
     height: 0.7,
-    types: ["Grass", "Poison"]
-    }
+    types: ["Grass", "Poison"]},
+
   { name: "Weepinbell",
     height: 1,
-    types: ["Grass", "Posion"]
-    }
+    types: ["Grass", "Posion"]},
+
   { name: "Victreebel",
     height: 1.7,
-    types: ["Grass", "Poison"]
-    }
+    types: ["Grass", "Poison"]},
 ];
+
+// Print a list of all objects in pokemonList with their name and height.
+for (let i=0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
+  if (pokemonList[i].height > 1) {
+    document.write(" - That's one TALL Pokemon!")
+  }
+  document.write("<br/>")
+}
