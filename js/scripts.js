e474091e6fc6de3e23d9dc4a6d905f602742ceca
LukeHTHompson <lht14dv@icloud.com>
2021-08-18 11:27:14 -0400

    <br/> to <p> for DOM
    Make array info print in format of paragraphs in DOM for future styling ease

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,17 @@ let pokemonList = [
 ];
 
 // Print a list of all objects in pokemonList with their name and height.
-for (let i=0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ")")
-  if (pokemonList[i].height > 1) {
-    document.write(" - That's one TALL Pokemon!")
+// Each new line is a new paragrpah in the DOM. Alternate could be line 23 <br>
+function printPokemon(){
+  for (let i=0; i < pokemonList.length; i++) {
+    document.write("<p>" + pokemonList[i].name + " (height: " + pokemonList[i].height + ")");
+    if (pokemonList[i].height > 1) {
+      document.write(" - That's one TALL Pokemon!");
+    }
+    document.write("</p>");
+    //document.write("<br/>");
   }
-  document.write("<br/>")
 }
+
+//Print Pokemon list once
+printPokemon();
