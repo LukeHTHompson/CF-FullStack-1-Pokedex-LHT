c7ef17cbb15f3557f48257ba1ef07d935aae95f1
LukeHTHompson <lht14dv@icloud.com>
2021-08-19 15:50:44 -0400

    API Data Return and Display

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,20 @@
 let pokemonRepository = (function () {
   // Create an empty array to populate in returns
   let pokemonList = [];
+  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=898"
 
   // pokemonRepository.add(objectName) will add the object to pokemonList.
   // must be validated as an object with proper keys first.
   function add (pokemonObject) {
     if (
-      (typeof (pokemonObject) === "object") &&
-      Object.keys(pokemonObject).every((arrayItem) =>
-      ["number", "name", "height", "types"].includes(arrayItem)
-    )
-  ) { pokemonList.push(pokemonObject); }
+      (typeof (pokemonObject) === "object")
+      // The validation on the next few lines was only for the test
+      // array of pokemon that I setup, but the JSON will not pass this.
+      //   &&
+      //   Object.keys(pokemonObject).every((arrayItem) =>
+      //   ["number", "name", "height", "types"].includes(arrayItem)
+      // )
+  ) { pokemonList.push(pokemonObject) }
   }
 
   //pokemonRepository.getAll() will return contents of pokemonList
@@ -23,9 +27,14 @@ let pokemonRepository = (function () {
   // since # and name are the contents of the buttons, and our
   // event listener is set up on those buttons, use event.target
   // to find which button was pressed, then print its contents
-  function showDetails (event) {
-    let details = event.target.innerText;
-    console.log(details);
+  function showDetails (pokemon) {
+    let pokemonLogData = event.target.innerText + ":";
+    let pokemonDetails = pokemon.detailsUrl;
+    console.log(pokemon);
+    loadDetails(pokemon/*Details)*/).then(function () {
+      console.log(pokemonLogData);
+      console.log(pokemon);
+    });
   }
 
   // Add listener to new pokemon buttons after adding them.
@@ -40,101 +49,90 @@ let pokemonRepository = (function () {
   function addListItem (pokemon) {
     let list = document.querySelector("ul");
     let button = document.createElement('button');
-    button.innerText = "#" + pokemon.number + " - " + pokemon.name;
+    // Format the pokemon names from the JSON to capitalize first letter
+    let nameFormat = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+    button.innerText = nameFormat;
     button.classList.add("pokemon-buttons")
     list.appendChild(button);
     newButtonListener(button);
   }
 
+  function loadList() {
+    // return the json plain text file as a promise object via fetch
+    return fetch(apiUrl).then(function (response) {
+      // we can use then, since fetch is a promise
+      // this object is the plain text reformatted as a json via .json()
+      // .json() also returns a promise object so we can keep using .then
+      return response.json();
+    }).then(function (json) {
+      // look at the results of the json now that it is formatted as json
+      // for each item make a pokemon object with the name and url from the item
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        // add that pokemon object to our list before continuing to the next item
+        // add can be called directly here since we are still within the IIFE
+        add(pokemon);
+      });
+      // Next we use a catch in case the json was not formatted correctly
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  // Similar logic for then chains and data acquisition as loadList above
+  function loadDetails(item) {
+    let url= item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Here we have our json formatted data and can add it to the pokemon
+      // The format of the values we are assigning is based on the json data
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      item.number = details.id;
+      // Next we use a catch in case the json was not formatted correctly
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  // Function to Search for Pokemon
+  function search(searchString) {
+    return pokemonRepository.getAll().filter(
+      // Using the next pokemon in the list, return the name, make it toLowerCase
+      // then check if the searchString in lowercase is a part of the pokemon name we are on
+      // perform this for all pokemon in the list to find all that match.
+      (nextPokemonInList) => nextPokemonInList.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1
+    );
+  }
+
   return {
     add: add,
     getAll: getAll,
-    addListItem: addListItem
+    addListItem: addListItem,
+    loadList: loadList,
+    loadDetails: loadDetails,
+    search: search
   };
 }) ();
 
-//Setup Pokemon Objects for with pokemonRepository.add(object)
-let Bellsprout = {
-  number: 69,
-  name: "Bellsprout",
-  height: 0.7,
-  types: ["Grass", "Poison"]
-};
+// Start by loading the pokemon to our list
+pokemonRepository.loadList().then(function () {
+  // At this point we should have all our pokemon data
+  // Now go one by one and add the pokemon to our HTML unordered list
+  pokemonRepository.getAll().forEach(function (pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
+});
 
-let Weepinbell = {
-  number: 70,
-  name: "Weepinbell",
-  height: 1,
-  types: ["Grass", "Poison"]
-};
-
-let Victreebel = {
-  number: 71,
-  name: "Victreebel",
-  height: 1.7,
-  types: ["Grass", "Poison"]
-};
-
-// Populate some data in pokemonList via pokemonRepository.add(object)
-pokemonRepository.add(Bellsprout);
-pokemonRepository.add(Weepinbell);
-pokemonRepository.add(Victreebel);
-
-// Function to search for pokemon
-// function search(input) {
-//   return pokemonRepository.getAll().filter(input => pokemonRepository.getAll().name === input);
-// };
-
-// Function to Search for Pokemon
-function search(searchString) {
-  return pokemonRepository.getAll().filter(
-    // Using the next pokemon in the list, return the name, make it toLowerCase
-    // then check if the searchString in lowercase is a part of the pokemon name we are on
-    // perform this for all pokemon in the list to find all that match.
-    (nextPokemonInList) => nextPokemonInList.name.toLowerCase().indexOf(searchString.toLowerCase()) > -1
-  );
-}
-
-// Replaced by element creation loop to supply pokemon to webpage: createPokemon
-
-// Functions to print pokemon info
-// function printNumberSpace(pokemon) {
-//   document.write("#" + pokemon.number + " ")
-// };
-//
-// function printNameSpace(pokemon) {
-//   document.write(pokemon.name + " ")
-// };
-//
-// function printHeight(pokemon) {
-//   document.write("height: " + pokemon.height)
-// };
-//
-// // Check height of pokemon based on threshold 1
-// // Append extra comment for pokemon taller than 1
-// function checkHeight(pokemon) {
-//   if (pokemon.height > 1) {
-//     document.write(" - That's one TALL Pokemon!")
-//   }
-// };
-//
-// function printPokemon() {
+// Redundant because of lines 82-89
+// function createPokemon () {
 //   pokemonRepository.getAll().forEach(function(pokemon) {
-//     document.write("<p>")
-//     printNumberSpace(pokemon);
-//     printNameSpace(pokemon);
-//     printHeight(pokemon);
-//     checkHeight(pokemon);
-//     document.write("</p>")
-//   });
+//     pokemonRepository.addListItem(pokemon)
+//   })
 // }
-
-function createPokemon () {
-  pokemonRepository.getAll().forEach(function(pokemon) {
-    pokemonRepository.addListItem(pokemon)
-  })
-}
-
-// Print all Pokemon data.
-// printPokemon();
-createPokemon();
+console.log(pokemonRepository.getAll())
