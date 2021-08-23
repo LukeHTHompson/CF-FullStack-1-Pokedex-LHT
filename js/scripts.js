460db47c29a31cb5955c1387325f7a07093d82ec
LukeHTHompson <lht14dv@icloud.com>
2021-08-23 10:28:02 -0400

    Code Clean Up + Object Validation

--- js/scripts.js
+++ js/scripts.js
@@ -8,39 +8,18 @@ let pokemonRepository = (function () {
   // must be validated as an object with proper keys first.
   function add (pokemonObject) {
     if (
-      (typeof (pokemonObject) === "object")
-      // The validation on the next few lines was only for the test
-      // array of pokemon that I setup, but the JSON will not pass this.
-      //   &&
-      //   Object.keys(pokemonObject).every((arrayItem) =>
-      //   ["number", "name", "height", "types"].includes(arrayItem)
-      // )
+      (typeof (pokemonObject) === "object") &&
+        Object.keys(pokemonObject).every((arrayItem) =>
+        ["name", "detailsUrl"].includes(arrayItem)
+      )
     ) { pokemonList.push(pokemonObject) }
-  }
-
-  //pokemonRepository.getAll() will return contents of pokemonList
-  function getAll () {
-    return pokemonList;
-  }
-
-  // Log to the console the pokemon's # and name when clicked
-  // since # and name are the contents of the buttons, and our
-  // event listener is set up on those buttons, use event.target
-  // to find which button was pressed, then print its contents
-  function showDetails (pokemon) {
-    // let pokemonLogData = event.target.innerText + ":";
-    loadDetails(pokemon).then(function () {
-      // console.log(pokemonLogData);
-      // console.log(pokemon);
-    });
   }
 
   // Add listener to new pokemon buttons after adding them.
   function newButtonListener (button, pokemon) {
-    // Log the pokemon's # and name to console when clicked
-    // Use an unnamed function that calls into showDetails with the pokemon
-    // whose button we are currently clicking
-    button.addEventListener("click", function() { showDetails(pokemon) } );
+    button.addEventListener("click", function() {
+      showDetails(pokemon)
+    } );
   }
 
   // Add a button to the end of the UI for each pokemon in the repository
@@ -49,8 +28,7 @@ let pokemonRepository = (function () {
   function addListItem (pokemon) {
     let list = document.querySelector("ul");
     let button = document.createElement('button');
-    // Format the pokemon names from the JSON to capitalize first letter
-    // Commented out as example of how to do this JS, but can also be done in CSS:
+    // Format the pokemon names from the JSON to capitalize first letter:
     // CSS: text-transform: capitalize; will capitalize first letter of each word
     // JS:  let nameFormat = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     button.innerText = pokemon.name;
@@ -58,6 +36,46 @@ let pokemonRepository = (function () {
     button.id = pokemon.name;
     list.appendChild(button);
     newButtonListener(button, pokemon);
+  }
+
+  // Similar logic for then chains and data acquisition as loadList above
+  function loadDetails(item) {
+    let url= item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      // Here we have our json formatted data and can add it to the pokemon
+      // The format of the values we are assigning is based on the json data
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      item.number = details.id;
+      // Setup info to be displayed in modal
+      let typeString = item.types.map((item) => item.type.name).join("/");
+      let modalTitle = "${item.name} (${typeString})";
+
+      let modalDetails =
+      "Height: " + item.height +
+      heightCheck(item.height) +
+      "<br/>" +
+      "<img src=" + item.imageUrl + ">";
+
+      showModal (modalTitle, modalDetails);
+      // showModal (item.name, (item.height + "<br/>" + "<img src=" + item.imageUrl + ">"));
+
+      // Next we use a catch in case the json was not formatted correctly
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
+
+  // print data from the API about this pokemon
+  function showDetails (pokemon) {
+    // let pokemonLogData = event.target.innerText + ":";
+    loadDetails(pokemon).then(function () {
+      // console.log(pokemonLogData);
+      // console.log(pokemon);
+    });
   }
 
   function loadList() {
@@ -85,6 +103,11 @@ let pokemonRepository = (function () {
     })
   }
 
+  //pokemonRepository.getAll() will return contents of pokemonList
+  function getAll () {
+    return pokemonList;
+  }
+
   function heightCheck (height) {
     // Looked up that average pokemon heigh is about 4, so 10 really is tall!
     if (height >= 10) {
@@ -93,39 +116,6 @@ let pokemonRepository = (function () {
     else {
       return ""
     }
-  }
-
-  // Similar logic for then chains and data acquisition as loadList above
-  function loadDetails(item) {
-    let url= item.detailsUrl;
-    return fetch(url).then(function (response) {
-      return response.json();
-    }).then(function (details) {
-      // Here we have our json formatted data and can add it to the pokemon
-      // The format of the values we are assigning is based on the json data
-      item.imageUrl = details.sprites.front_default;
-      item.height = details.height;
-      item.types = details.types;
-      item.number = details.id;
-      // Setup info to be displayed in modal
-      // console.log(item.types);
-      // let typeString = item.types.valueOf()[0].name;
-      let modalTitle = item.name
-      // + " (" + typeString + ")";
-
-      let modalDetails =
-      "Height: " + item.height +
-      heightCheck(item.height) +
-      "<br/>" +
-      "<img src=" + item.imageUrl + ">";
-
-      showModal (modalTitle, modalDetails);
-      // showModal (item.name, (item.height + "<br/>" + "<img src=" + item.imageUrl + ">"));
-
-      // Next we use a catch in case the json was not formatted correctly
-    }).catch(function (e) {
-      console.error(e);
-    });
   }
 
   // Function to Search for Pokemon
